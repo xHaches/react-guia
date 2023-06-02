c4ff34815c5b8d46ea23e06cc9404e7984b4d97c
Luis Hernandez <mluishe@gmail.com>
2023-06-02 13:59:04 -0600

    inicio de configuracion de gql

--- crm-react/src/main.tsx
+++ crm-react/src/main.tsx
@@ -5,6 +5,31 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router';
 import { Provider } from 'react-redux'
 import { store } from './store/store';
+import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client/core';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.VITE_API_URL
+  }),
+  cache: new InMemoryCache()
+});
+
+const query = gql`
+  query {
+    allClientes {
+        id
+        nombre
+        telefono
+        email
+        empresa
+    }
+  }
+`;
+
+client.query({
+  query
+}).then(res => console.log(res.data)
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
